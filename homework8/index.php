547f1ba15973d212dfb11021a90105ef2b47cb65
Ilia Ponomarev <seikadi123@gmail.com>
2019-02-19 20:36:04 +0300

    homework8

---
+++ homework8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+      <meta charset="UTF-8">
+      <title>Домашнее задание №8. Задача №1</title>
+      <link rel="stylesheet" href="">
+  </head>
+  <body>
+
+  <button type="submit" id="get">Показать все товары</button>
+  <button type="submit" id="create">Создать</button>
+
+  <div id="goods"></div>
+
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/main.js"></script>
+  </body>
+</html>
